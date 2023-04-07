4029646c35f56fb9491fd7ba8b8038dddc059e08
Narendarkatta <k.narendarreddy111@gmail.com>
2023-04-07 11:19:06 -0500

    Updated kanpsack.py
    Using random values between 1 and max val and weights between 1 and max wt, code creates a list of n items.

--- knapsack.py
+++ knapsack.py
@@ -1,5 +1,22 @@
+import random
+
 class Item:
     def __init__(self, value, weight):
         self.value = value
         self.weight = weight
-        
+
+def gen_items(n, max_wt, max_val):
+    items = []
+    for i in range(n):
+        value = random.randint(1, max_val)
+        weight = random.randint(1, max_wt)
+        item = Item(value, weight)
+        items.append(item)
+    return items
+
+nIn = 10
+maxWtIn = 100
+maxValIn = 50
+items = gen_items(nIn, maxWtIn, maxValIn)
+for item in items:
+    print(f"Value: {item.value}, Weight: {item.weight}")
