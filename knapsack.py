775985ce90503179c9c2a6de85a9508a5ad7c4b1
Jaichand Mulakalapalli <mjaichandms@gmail.com>
2023-04-07 12:21:53 -0500

    Add code to get input arguments item count and max weight

--- knapsack.py
+++ knapsack.py
@@ -1,4 +1,20 @@
 import random
+import sys
+
+if len(sys.argv) < 3 or sys.argv[1] == "" or sys.argv[2] == "":
+    print("Not enough input arguments provided. Exiting the program...")
+    sys.exit()
+else:
+    try:
+        item_cnt = int(sys.argv[1])
+    except ValueError:
+        print("Argument 1 is not an integer. Exiting the program...")
+        sys.exit()
+    try:
+        item_max_wt = int(sys.argv[2])
+    except ValueError:
+        print("Argument 2 is not an integer. Exiting the program...")
+        sys.exit()
 
 class Item:
     def __init__(self, value, weight):
@@ -14,9 +30,10 @@ def gen_items(n, max_wt, max_val):
         items.append(item)
     return items
 
-nIn = 10
-maxWtIn = 100
+nIn = item_cnt
+maxWtIn = item_max_wt
 maxValIn = 50
 items = gen_items(nIn, maxWtIn, maxValIn)
+print(f"No. of Items: {len(items)}")
 for item in items:
     print(f"Value: {item.value}, Weight: {item.weight}")
