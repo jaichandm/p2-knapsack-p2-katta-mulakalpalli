f0047b129aac21d1e298affb8356e72fb340c0d9
Jaichand Mulakalapalli <mjaichandms@gmail.com>
2023-04-11 21:13:54 -0500

    Greedy Heuristic: Sort by desc value update

--- knapsack.py
+++ knapsack.py
@@ -30,10 +30,30 @@ def gen_items(n, max_wt, max_val):
         items.append(item)
     return items
 
+def sort_val(items):
+    return sorted(items, key=lambda x: x.value, reverse=True)
+
+def desc_value_items(items, max_wt):
+    desc_val_items = []
+    total_value = 0
+    sorted_items = sort_val(items)
+    for item in sorted_items:
+        if item.weight <= max_wt:
+            desc_val_items.append(item)
+            max_wt -= item.weight
+            total_value += item.value
+    return desc_val_items, total_value
+
 nIn = item_cnt
 maxWtIn = item_max_wt
 maxValIn = 50
 items = gen_items(nIn, maxWtIn, maxValIn)
 print(f"No. of Items: {len(items)}")
 for item in items:
+    print(f"Value: {item.value}, Weight: {item.weight}")
+
+
+desc_val_items, total_value = desc_value_items(items, maxWtIn)
+print(f"\nSolution 1: Take as many items as possible with highest value first\nTotal value: {total_value}")
+for item in desc_val_items:
     print(f"Value: {item.value}, Weight: {item.weight}")
